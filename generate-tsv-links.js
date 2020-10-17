a8a563c14250abcf5a98947a5cabf5581b4cf087
Xandy <xands@outlook.com>
2020-10-17 15:55:00 +0800

    Update

--- generate-tsv-links.js
+++ generate-tsv-links.js
@@ -14,7 +14,8 @@ ZOHO.embeddedApp.on("PageLoad", async function(data){
       pageNum++
       dataArr.push(...response.data)
     }
-    updateDownloadHref(dataArr)
+    const filtered = dataArr.filter(item => data.EntityId.includes(item.id))
+    updateDownloadHref(filtered)
   } catch (err) {
     console.log({ err })
   }
@@ -26,50 +27,54 @@ function generateCsv(data) {
   const csvRows = data.map((item) => {
     const propertyOwner = item.Property_Owners ? JSON.parse(item.Property_Owners)[0] : null
     const propertyContact = item.Property_Contacts ? JSON.parse(item.Property_Contacts)[0] : null
+    const postalStreet = propertyOwner?.Postal_Address?.split(',') || ['']
     const info = {
       'Property Name': item.Deal_Name,
-      'Owner': propertyOwner ? propertyOwner.Company : null,
-      'Attention': propertyOwner ? propertyOwner.Salutation_Dear : null,
-      'Owner - Postal Street': propertyOwner ? propertyOwner.Postal_Street : null,
-      'Owner - Postal State': propertyOwner ? propertyOwner.Postal_State : null,
-      'Owner - Postal Postcode': propertyOwner ? propertyOwner.Postal_Postcode : null,
-      'Related Contact': propertyContact ? propertyContact.Name : null,
-      'Related Contact Salutation': propertyContact ? propertyContact.Salutation_Dear : null,
-      'Agent Name': item.Sales_Agent,
-      'Date to Start': item.Date_to_Start,
-      'Property Description': item.Property_Description,
-      'Location Description': item.Loaction_Description,
-      'Zoning': item.Prop_Zoning,
-      'Property - Council': item.Council,
-      'Property - Land Area': item.Land_Area_sqm,
-      'Property - Warehouse': item.Warehouse_sqm,
-      'Property - Office': item.Office_area_sqm,
-      'Property - Showroom': item.Showroom_sqm,
-      'Property - Retail': item.Retail_sqm,
-      'Access': item.Access,
-      'Property - Car Spaces': item.Car_Spaces,
-      'Current Use': item.Current_Use,
-      'Auction Date and Time': item.Auction_Date_Time1,
-      'Target Market': item.Target_Market,
-      'Sale Price': item.Sale_Price,
-      'Appeal': item.Appeal
+      'Owner': propertyOwner ? propertyOwner.Company || '' : '',
+      'Attention': propertyOwner ? propertyOwner.Salutation_Dear || '' : '',
+      'Owner - Postal Street': postalStreet[0],
+      'Owner - Postal Suburb': propertyOwner ? propertyOwner.Postal_Suburb || '' : '',
+      'Owner - Postal State': propertyOwner ? propertyOwner.Postal_State || '' : '',
+      'Owner - Postal Postcode': propertyOwner ? propertyOwner.Postal_Postcode || '' : '',
+      'Related Contact': propertyContact ? propertyContact.Name || '' : '',
+      'Related Contact Salutation': propertyContact ? propertyContact.Salutation_Dear || '' : '',
+      'Agent Name': item.Sales_Agent?.name || '',
+      'Date to Start': item.Date_to_Start || '',
+      'Property Description': item.Property_Description || '',
+      'Location Description': item.Loaction_Description || '',
+      'Zoning': item.Prop_Zoning?.toString() || '',
+      'Property - Council': item.Council || '',
+      'Property - Land Area': item.Land_Area_sqm || '',
+      'Property - Warehouse': item.Warehouse_sqm || '',
+      'Property - Office': item.Office_area_sqm || '',
+      'Property - Showroom': item.Showroom_sqm || '',
+      'Property - Retail': item.Retail_sqm || '',
+      'Access': item.Access || '',
+      'Property - Car Spaces': item.Car_Spaces || '',
+      'Current Use': item.Current_Use || '',
+      'Auction Date and Time': item.Auction_Date_Time1 ? moment(item.Auction_Date_Time1).format('DD/MM/YYYY hh:mmA') : '',
+      'Target Market': item.Target_Market || '',
+      'Sale Price': item.Estimated_Sale_Price || '',
+      'Appeal': item.Appeal || ''
     }
     return Object.values(info).map((leadValue) => {
       const string = JSON.stringify(leadValue)
       return string
     }).join(',')
   }).join('\n');
-  const csvHeaders = 'Property Name,Owner,Attention,Owner - Postal Street,Owner - Postal State,Owner - Postal Postcode,Related Contact,Related Contact Salutation,Agent Name,Date to Start,Property Description,Location Description,Zoning,Property - Council,Property - Land Area,Property - Warehouse,Property - Office,Property - Showroom,Property - Retail,Access,Property - Car Spaces,Current Use,Auction Date and Time,Target Market,Sale Price,Appeal';
+  const csvHeaders = 'Property Name,Owner,Attention,Owner - Postal Street,Owner - Postal Suburb,Owner - Postal State,Owner - Postal Postcode,Related Contact,Related Contact Salutation,Agent Name,Date to Start,Property Description,Location Description,Zoning,Property - Council,Property - Land Area,Property - Warehouse,Property - Office,Property - Showroom,Property - Retail,Access,Property - Car Spaces,Current Use,Auction Date and Time,Target Market,Sale Price,Appeal';
   return `${csvHeaders}\n${csvRows}\n`;
 }
 
 function updateDownloadHref(data) {
   // Create a binary representation of the plain-text input.
   const csvData = generateCsv(data);
+  const csvDataReplaced = csvData.replace(/–/g, '-')
   const blob = new Blob(
-      [csvData], // Blob parts.
+      [csvDataReplaced], // Blob parts.
       {
-          type : "text/csv;charset=utf-8"
+          type : "text/csv;charset=UTF-8",
+          encoding:"UTF-8"
       }
   );
   document.querySelector('.hidden').classList.remove('hidden')
